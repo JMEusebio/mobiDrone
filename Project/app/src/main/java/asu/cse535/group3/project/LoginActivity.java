ebc4cb21e61a9f0cfec82cc82feae3cc3754c171
ajain64 <ajain64@asu.edu>
2017-04-30 07:54:08 -0700

    Update LoginActivity.java

--- Project/app/src/main/java/asu/cse535/group3/project/LoginActivity.java
+++ Project/app/src/main/java/asu/cse535/group3/project/LoginActivity.java
@@ -14,52 +14,97 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     private DatabaseHandler dh;
+    private String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
 
         dh = new DatabaseHandler(this);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
-        final TextView tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
-        final Button bLogin = (Button) findViewById(R.id.bSignIn);
 
-        tvRegisterLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                LoginActivity.this.startActivity(registerIntent);
-            }
-        });
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            username = getIntent().getStringExtra("USERNAME");
+        }
 
-        bLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String username = etUsername.getText().toString();
-                final String password = etPassword.getText().toString();
+        if (username.equals("")) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+            final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+            final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+            final TextView tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
+            final Button bLogin = (Button) findViewById(R.id.bSignIn);
 
-                if (!dh.isLoginCorrect(username, password))
-                {
-                    builder.setMessage("Username and/or password is incorrect.")
-                            .setNegativeButton("Retry", null)
-                            .create()
-                            .show();
+            tvRegisterLink.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    LoginActivity.this.startActivity(registerIntent);
                 }
-                else
-                {
-                    builder.setMessage("Thank you for logging in.")
+            });
+
+            bLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final String username = etUsername.getText().toString();
+                    final String password = etPassword.getText().toString();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+                    if (!dh.isLoginCorrect(username, password)) {
+                        builder.setMessage("Username and/or password is incorrect.")
+                                .setNegativeButton("Retry", null)
+                                .create()
+                                .show();
+                    } else {
+                        builder.setMessage("Thank you for logging in.")
+                                .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                        Intent settingsIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                        settingsIntent.putExtra("USERNAME", username);
+                                        startActivity(settingsIntent);
+
+                                    }
+                                })
+                                .create()
+                                .show();
+                    }
+                }
+
+            });
+        } else {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.logged_in);
+            TextView welcome = (TextView) findViewById(R.id.welcome);
+            welcome.setText("Welcome, " + username);
+
+            Button home = (Button) findViewById(R.id.loggedInRedirect);
+            home.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("USERNAME", username);
+                    LoginActivity.this.startActivity(intent);
+                }
+            });
+
+            Button logout = (Button) findViewById(R.id.logout);
+            logout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+
+                    builder.setMessage("You have been logged out.")
                             .setPositiveButton("Continue", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
 
                                     Intent settingsIntent = new Intent(LoginActivity.this, MainActivity.class);
                                     startActivity(settingsIntent);
-
                                 }
                             })
                             .create()
@@ -67,10 +112,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
                 }
-
-
-            }
-
-        });
+            });
+        }
     }
 }
